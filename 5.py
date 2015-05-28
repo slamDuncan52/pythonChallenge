3108e8f3512358cf011b2b7e8ca3a2c02b906b70
Mitchell Duncan <mitchell.duncan@magnetimarelli.com>
2015-05-28 16:07:29 -0400

    Answer to number 5, my favorite yet

---
+++ 5.py
@@ -0,0 +1,10 @@
+import pickle
+import urllib2
+res = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/banner.p")
+data = res.read()
+decode = pickle.loads(data)
+for item in decode:
+    answer = ""
+    for pair in item:
+        answer += pair[0] * pair[1]
+    print answer
