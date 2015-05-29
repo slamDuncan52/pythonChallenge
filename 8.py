6bfa47798ae5c11a50da12388aa1ac251d2c3bc7
Mitchell Duncan <mitchell.duncan@magnetimarelli.com>
2015-05-29 14:00:15 -0400

    Okay 8 was bullshit not gonna lie

---
+++ 8.py
@@ -0,0 +1,12 @@
+import urllib2
+import re
+import bz2
+
+data = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/integrity.html")
+text =  data.read()
+user = re.findall("un: '(.+)'\npw:",text)
+pswd = re.findall("pw: '(.+)'\n-->",text)
+newuser = bz2.decompress(user[0].decode('string_escape'))
+newpswd = bz2.decompress(pswd[0].decode('string_escape'))
+print newuser
+print newpswd
