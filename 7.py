88463958b532732577b2aa5918510821c2e0c5b6
Mitchell Duncan <mitchell.duncan@magnetimarelli.com>
2015-05-29 12:48:03 -0400

    Made 7's solution longer and much more robust. It doesn't assume much about the image now, and it programmatically finds the end of the encode blocks, and the block size!

--- 7.py
+++ 7.py
@@ -2,22 +2,46 @@ import urllib2
 import re
 import os
 from scipy import misc
+from scipy.stats import mode
 
 data = urllib2.urlopen("http://www.pythonchallenge.com/pc/def/oxygen.png")
 with open("oxygen.png","wb") as imFile:
     imFile.write(data.read())
 
 imArr = misc.imread("oxygen.png")
+os.remove("oxygen.png")
 
 penult = ""
-prev = " "
+mid = len(imArr)/2
+blkEnd = 0
 
-for i in range(0,608,7):
-    penult +=chr(imArr[47][i][0])
+for pixel in imArr[mid]:
+    if(pixel[0] != pixel[1]):
+        break
+    blkEnd += 1
+
+cur = 0
+prev = 0
+tmpSize = 1
+blkSize = 1
+blkList = []
+
+for col in range(0,blkEnd):
+    cur = imArr[mid][col][0]
+    if(cur == prev):
+        tmpSize += 1
+    else:
+        blkList.append(tmpSize)
+        tmpSize = 1
+        prev = cur
+
+blkSize = mode(blkList)[0]
+
+for col in range(0,blkEnd,blkSize):
+    penult +=chr(imArr[mid][col][0])
 print penult
 
 finalArr = []
-for thing in re.findall("([0-9]+)",penult):
-    finalArr.append(chr(int(thing)))
+for num in re.findall("([0-9]+)",penult):
+    finalArr.append(chr(int(num)))
 print "".join(finalArr)
-os.remove("oxygen.png")
