56a2803367c63f920f6e5a67e2c7e150adbf3d50
Mitchell Duncan <mitchell.duncan@magnetimarelli.com>
2015-06-01 15:41:39 -0400

    Alright 13 might be broken, because I'm damn sure there's an xml system server there. But maybe just the proxy I'm on is breaking it #fuckThisShit

---
+++ 13.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import urllib.request
+import xmlrpc.client
+
+site = "www.pythonchallenge.com/pc/phonebook.php"
+auth_handle = urllib.request.HTTPBasicAuthHandler()
+auth_handle.add_password(realm="inflate",
+        uri="http://"+site,
+        user="huge",
+        passwd="file")
+urllib.request.install_opener(urllib.request.build_opener(auth_handle))
+
+xmlMsngr = xmlrpc.client.ServerProxy("http://huge:file@"+site)
+
+print(xmlMsngr.system.methodHelp('phone'))
