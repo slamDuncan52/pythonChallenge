dd474dce79b34e25dfba9dc7b7c58763df5bd874
Mitchell Duncan <mitchell.duncan@magnetimarelli.com>
2015-06-08 13:03:14 -0400

    I'm using fancy print formats now!

--- 15.py
+++ 15.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import datetime
 import calendar
-for year in range(1586,1996,10):
-    if(calendar.isleap(year) and datetime.date(year,1,1).weekday() == 3):
-        print(year)
-print("Who was born on January 27th 1756?")
+out = [year for year in range(1586,1996,10) 
+       if(calendar.isleap(year) and datetime.date(year,1,1).weekday() == 3)]
+print("Who was born on January 27th {0[0]}?".format(out))
 print("(Hint: It's Mozart)")
