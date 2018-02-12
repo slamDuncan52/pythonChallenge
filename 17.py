1066242cf33e4098fc9889f35b3c3eaa67388ec4
Mitchell Duncan <dunc0474@kettering.edu>
2018-02-12 01:59:37 -0500

    I hate 17 but i did it

--- 17.py
+++ 17.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-import re
+import re, bz2
 import urllib.request as urllib
-import http.cookiejar
+import xmlrpc.client
+from urllib.parse import unquote_to_bytes
 
 site = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
+auth_handle = urllib.HTTPBasicAuthHandler()
+auth_handle.add_password(realm='inflate',
+         uri=site,
+         user='huge',
+         passwd='file')
+urllib.install_opener(urllib.build_opener(auth_handle))
+
 def chainFollow(nothingValue):
-    index = 0
-    seen = []
+    seen = ''
     print("Working...")
-    while 1:
-        with urllib.urlopen(site+nothingValue) as response:
-            cookie = dict(response.info())["Set-Cookie"]
-            data = response.read()
-            codec = response.info().get_param('charset','utf-8')
-            data = data.decode(codec)
-        index = index + 1
+    while True:
+        response = urllib.urlopen(site+nothingValue)
+        cookie = re.findall("(info=)(.*?)(;)",dict(response.info())["Set-Cookie"])
+        data = str(response.read())
         found = re.findall("(next busynothing is )([0-9]+)",data)
+        seen += cookie[0][1]
         if(len(found) > 0):
             nothingValue = found[0][1]
-            print(str(index) + ": " + nothingValue)
-            if(nothingValue in seen):
-                break
-            else:
-                seen.append(nothingValue)
         else:
-            print("Finished: "+ data)
+            print(bz2.decompress(unquote_to_bytes(seen.replace("+"," "))).decode())
             break
-chainFollow("6711")
+chainFollow("12345")
+xmlMsngr = xmlrpc.client.ServerProxy("http://huge:file@www.pythonchallenge.com/pc/phonebook.php")
+print(xmlMsngr.phone("Leopold"))
